949aba8628b4a6dd54626eaa707cd10fd638be43
ayunekosuke-cmyk <ayunekosuke@gmail.com>
2026-04-20 23:11:59 +0900

    Update works.js

--- js/works.js
+++ js/works.js
@@ -1,75 +1,165 @@
 
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>NAMAYU88 PORTFOLIO</title>
+  <meta name="description" content="テキストテキストテキストテキストテキストテキストテキストテキスト">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="img/favicon (2).ico">
+  <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css">
 
 
 
+</head>
+
+<body class="theme-works">
+ <!-- ここからヘッダー-->
+  <header>  
+
+<!-- ヘッダーロゴ（左上用） -->
+<a href="../index.html" class="logo">TOP</a>
+
+  </header>
+  <!-- ここからコンテナ（フッターまで） -->
+  <div class="container">
+    <!-- ここからメイン（フッターより上） -->
+    <main>
+
+        <!-- ここからワークスセクション -->
+<section id="works">
 
 
+  <h2 class="section-title">
+  <div class="section-title-inner">
+    <span class="title-text">works</span>
+    <span class="dots">
+      <span></span><span></span><span></span>
+      <span></span><span></span><span></span>
+    </span>
+  </div>
+</h2>
+<div class="worksA"><a href="../index.html">トップページに戻る＞</a></div>
+  <div class="works-list fade-up">
+    <article class="work-card" data-target="work1">
+      <img src="../img/futuresum.png" alt="作例1サムネイル">
+      <div class="work-card-content">
+        <h3 class="work-card-title">
+            <span>作例１</span>
+          </h3>
+        <p>Webサイト制作（デザイン・コーディング）</p>
+        <a href="fyu-tya-rink/index.html" target="_blank" rel="noopener" class="site-link">デモサイトを見る＞</a>
+      </div>
+    </article>
+
+    <article class="work-card" data-target="work2">
+      <img src="../img/portLPsum.png" alt="作例2サムネイル">
+            <div class="work-card-content">
+      <h3 class="work-card-title">作例２</h3>
+      <p>企業サイトLP</p>
+      </div>
+    </article>
+
+    <article class="work-card" data-target="work3">
+      <img src="../img/rijingsum.png" alt="作例3サムネイル">
+      <div class="work-card-content">
+      <h3 class="work-card-title">作例３</h3>
+      <p>ワードプレスでのサイト制作例</p>
+      </div>
+    </article>
+
+    <article class="work-card" data-target="work4">
+      <img src="../img/byouinsum.png" alt="作例4サムネイル">
+      <div class="work-card-content">
+      <h3 class="work-card-title">作例４</h3>
+      <p>フィグマによるデザイン、病院サイト</p>
+      </div>
+    </article>
+  </div>
+
+  <div class="works-viewer fade-up">
+    <span class="close-btn">×</span>
+    <div class="work-detail is-active" id="work1">
+
+      <h4>
+        <span class="label">作例１</span>
+        <span class="main">Futurelink</span>
+        <span class="sub">福祉企業サイト</span>
+      </h4>
+      <p>福祉企業サイトを参考に、温かみと親しみやすさを意識したWebサイトを制作しました。
+HTML・CSS・JavaScriptを使用してコーディング、デザインをし、動きのあるデザインと視認性の高さを両立しています。
+リンク先に作例サイトがございます。レスポンシブ対応も行っています。
+<a href="fyu-tya-rink/index.html" target="_blank" rel="noopener" class="site-link">デモサイトを見る →＞</a>
+      </p>
+      <img src="../img/futyrerinc 1.jpg" alt="作例1の大きな画像">
+    </div>
+
+    <div class="work-detail is-active" id="work2">
+            <h4>
+        <span class="label">作例２</span>
+        <span class="main">LPデザイン</span>
+        <span class="sub">システムサイトトップ画像</span>
+      </h4>
+      <p>システムサイトのトップに表示するＬＰを制作しました。サイトのテーマカラーを参考に、視認性や視線誘導を意識し、緑枠ありとなしのデザインを考えました
+      </p>
+  
+      <img src="../img/portflp.jpg" alt="作例2の大きな画像">
+    </div>
+    <div class="work-detail is-active" id="work3">
+      <h4>
+              <span class="label">作例３</span>
+        <span class="main">ワードプレス制作</span>
+        <span class="sub">サイトトップ</span>
+      </h4>
+      <p>WordPressを用いたサイト制作例です。ワードプレスにてページの全てをデザインし、トップページにて記事をテーマ別に紹介しています。
+          <span class="close-btn">×</span>
+      </p>
+      <img src="../img/risingworksdetail22.png" alt="作例3の大きな画像">
+    </div>
+        <div class="work-detail is-active" id="work4">
+                      <h4>
+        <span class="label">作例４</span>
+        <span class="main">診療所サイトデザイン</span>
+        <span class="sub">topページ外来診療ページ</span>
+      </h4>
+          <p>Figmaを使用して病院サイトのデザインを制作しました。
+また、各ページのアイコンはIllustratorで内容に合わせて制作しています。
+          </p>
+  <span class="close-btn">×</span>
+      <img src="../img/shinryoutopgairai.png" alt="作例4の大きな画像">
+    </div>
+  </div>
+</section>
+  <!-- ここからフッター -->
+    </main>
+    <footer>
+      <div class="inner">
+              <ul>
+         <li><a href="https://crowdworks.jp/" target="_blank">仕事依頼（クラウドワークス）</a></li>
 
 
-$(function(){
-  const logo = $('.logo');
+        <li> NAMAYU88 PORTFOLIO</li>
+      </ul>
+      </div>
+    </footer>
+  </div>
 
-  let pastPos = 0;
-  const start = 300;
+  <!-- jQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-  $(window).on('scroll', function(){
-    const scroll = $(window).scrollTop();
+<!-- プラグイン -->
+<script src="../js/jquery.inview.min.js"></script>
 
-    // ▼ ロゴ制御（これを追加）
-    if(scroll > start){
-      logo.addClass('show');
-    } else {
-      logo.removeClass('show');
-    }
+<!-- slick -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
 
-    pastPos = scroll;
-  });
-});
-
-
-
-// モーダル
-$(function () {
-  const cards = $('.work-card');
-  const viewer = $('.works-viewer');
-  const details = $('.work-detail');
-
-  // カードクリック
-  cards.on('click', function () {
-    const target = $(this).data('target');
-
-    details.removeClass('is-active');
-    $('#' + target).addClass('is-active');
-
-    viewer.addClass('active');
-  });
-
-  // ×で閉じる
-  $('.close-btn').on('click', function () {
-    viewer.removeClass('active');
-  });
-
-  // 背景クリックでも閉じる
-  viewer.on('click', function (e) {
-    if ($(e.target).is('.works-viewer')) {
-      viewer.removeClass('active');
-    }
-  });
-});
-
-
-$(function() {
-  $(window).on('scroll load', function() {
-    $('.fade-up').each(function() {
-      const elemTop = $(this).offset().top;
-      const scroll = $(window).scrollTop();
-      const windowHeight = $(window).height();
-
-      if (scroll + windowHeight > elemTop + 80) {
-        $(this).addClass('show');
-      }
-    });
-  });
-});
-
-
+<!-- 自分 -->
+<script src="../js/works.js"></script>
+</body>
+</html>
